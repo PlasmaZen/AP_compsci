66e60fcee796b56ce9621d4c3fcf678049cbf232
PlasmaZen <31518912+PlasmaZen@users.noreply.github.com>
2024-04-11 09:25:31 -0600

    Add files via upload

--- dictionarystuff.py
+++ dictionarystuff.py
@@ -12,8 +12,18 @@ def make_dict():
         user_dict[dict_key] = dict_value
         print(user_dict)
 
-def merge_dict(dict1, dict2):
-    return(dict2.update(dict1))
+def append_list(user_dict):
+    while True:
+        dict_key = str(input("Input dictionary key (EXIT) "))
+        if dict_key == 'EXIT':
+            print(user_dict)
+            return user_dict
+        dict_value = str(input("input dictionary value (EXIT) "))
+        if dict_value == 'EXIT':
+            print(user_dict)
+            return user_dict
+        user_dict[dict_key] = dict_value
+        print(user_dict)
 
 def index_dict(dict):
     pass
@@ -27,12 +37,16 @@ def user_choice_switch(choice, user_dict):
         case 3:
             print(user_dict)
         case 4:
+            append_list(user_dict)
+        case 5:
             return False
         
 def user_menu():
     user_dict = {}
     while True:
-        user_choice = int(input('Welcome to Dict Maker - input choice: \n 1. Make Dictionary \n 2. Index Dictionary \n 3. Print Dictionary \n 4. EXIT '))
-        user_choice_switch(user_choice, user_dict)
+        user_choice = int(input('Welcome to Dict Maker - input choice: \n 1. Make Dictionary \n 2. Index Dictionary \n 3. Print Dictionary \n 4. EXIT \n'))
+        user_dict = user_choice_switch(user_choice, user_dict)
+        if user_dict is False:
+            break
 
 user_menu()
